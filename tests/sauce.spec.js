dd4c8658b662acd245ca530e0410617065d367d2
Ridoyabdulla <ridoy.ridoy005@gmail.com>
2025-11-24 00:50:06 +0600

    upload

--- tests/sauce.spec.js
+++ tests/sauce.spec.js
@@ -2,38 +2,38 @@
 import { test, expect } from '@playwright/test';
 
 test('log in ', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com');
+  await page.goto('https://www.saucedemo.com/');
 
   let userNameTxt = page.getByRole("textbox", {"name" :"Username"})
   let passwordTxt = page.getByRole("textbox", {"name" : "Password"})
+
+  //await page.waitForTimeout(4000);
   
 
   await userNameTxt.fill("standard_user");
-    await passwordTxt.fill("secret_sauce");
-    await page.getByRole('button', {name : "Login"}).click();
+  await passwordTxt.fill("secret_sauce");
+  await page.getByRole('button', {name : "Login"}).click();
+
+  await page.waitForTimeout(3000);
 
   await expect(page.locator('[data-test="title"]')).toBeVisible();
   await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
 
+   await page.waitForTimeout(2000);
+
   await page.locator('[data-test="shopping-cart-link"]').click();
   await page.locator('[data-test="item-4-title-link"]').click();
+
+   await page.waitForTimeout(2000);
 
   
   await page.getByRole('button', { name: 'Open Menu' }).click();
   await page.locator('[data-test="logout-sidebar-link"]').click();
     
-   //await page.getByRole("button", {name :"Add to cart"}).click();
-
   await page.pause();
 
-   //await page.getByRole("button", {name :"Add to cart"}).click();
-
-
-
-  //let addcarttxt = page.getByRole("button", {"name" :"Add to cart"})
 });
 
-//test('add cart')
 
 
 
